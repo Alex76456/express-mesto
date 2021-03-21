655634552b38828e39efab457fcba879154701de
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-03-22 00:02:27 +0300

    fix: убрать большинство ошибок линта

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-//const path = require('path');
+// const path = require('path');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => {
-	req.user = {
-		_id: '6054ea585885740a5cba70ed'
-	};
-	next();
+  req.user = {
+    _id: '6054ea585885740a5cba70ed',
+  };
+  next();
 });
 
 app.use(router);
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT, () => {
-	console.log(`App is running on port ${PORT}`);
+  console.log(`App is running on port ${PORT}`);
 });
